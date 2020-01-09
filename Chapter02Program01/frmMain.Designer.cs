84c7e502f5ccaa1b30f5af904daac5a40862bd21
Deidre <deidre.balard@outlook.com>
2020-01-08 19:30:57 -0500

    Class note

---
+++ Chapter02Program01/frmMain.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Chapter02Program01
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZIP = new System.Windows.Forms.TextBox();
+            this.txtDisplayOutput = new System.Windows.Forms.TextBox();
+            this.btnDisplayOuput = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(2, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address:";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(2, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(229, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "State";
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(342, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ZIP";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(96, 9);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(359, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(96, 42);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(359, 20);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(96, 77);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(127, 20);
+            this.txtCity.TabIndex = 7;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(276, 78);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(60, 20);
+            this.txtState.TabIndex = 8;
+            // 
+            // txtZIP
+            // 
+            this.txtZIP.Location = new System.Drawing.Point(388, 77);
+            this.txtZIP.Name = "txtZIP";
+            this.txtZIP.Size = new System.Drawing.Size(67, 20);
+            this.txtZIP.TabIndex = 9;
+            // 
+            // txtDisplayOutput
+            // 
+            this.txtDisplayOutput.Location = new System.Drawing.Point(2, 166);
+            this.txtDisplayOutput.Multiline = true;
+            this.txtDisplayOutput.Name = "txtDisplayOutput";
+            this.txtDisplayOutput.ReadOnly = true;
+            this.txtDisplayOutput.Size = new System.Drawing.Size(453, 127);
+            this.txtDisplayOutput.TabIndex = 10;
+            // 
+            // btnDisplayOuput
+            // 
+            this.btnDisplayOuput.Location = new System.Drawing.Point(2, 119);
+            this.btnDisplayOuput.Name = "btnDisplayOuput";
+            this.btnDisplayOuput.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplayOuput.TabIndex = 11;
+            this.btnDisplayOuput.Text = "&Display";
+            this.btnDisplayOuput.UseVisualStyleBackColor = true;
+            this.btnDisplayOuput.Click += new System.EventHandler(this.BtnDisplayOuput_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(380, 119);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 313);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDisplayOuput);
+            this.Controls.Add(this.txtDisplayOutput);
+            this.Controls.Add(this.txtZIP);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Mailing Label Program";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZIP;
+        private System.Windows.Forms.TextBox txtDisplayOutput;
+        private System.Windows.Forms.Button btnDisplayOuput;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ Chapter02Program01/frmMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter02Program01
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        [STAThread]
+        public static void Main()
+        {
+            frmMain main = new frmMain();
+            Application.Run(main);
+
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDisplayOuput_Click(object sender, EventArgs e)
+        {
+            String buffer;
+            buffer = "Mailing Label:" + Environment.NewLine +
+                Environment.NewLine;
+            buffer = buffer + "Name:" + txtName.Text +
+                Environment.NewLine;
+            buffer = buffer + "Address:" + txtAddress.Text +
+                Environment.NewLine;
+            buffer = buffer + "City:" + txtCity.Text +
+                Environment.NewLine;
+            buffer = buffer + "State:" + txtState.Text +
+                Environment.NewLine;
+            buffer = buffer + "ZIP:" + txtZIP.Text +
+                Environment.NewLine;
+            txtDisplayOutput.Text = buffer;
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
